0c54bfaa19b9374960fc680797d78b1dc7e0c7d5
Calvin Miracle <cbmira01@gmail.com>
2021-06-20 17:46:34 -0400

    Second draft of quick test

---
+++ kernels/small_multiply.cl
@@ -0,0 +1,10 @@
+
+// Perform an elementwise add on two small arrays
+__kernel void add(
+    __global const float *first_argument_g, 
+    __global const float *second_argument_g, 
+    __global float *result_g)
+{
+  int gid = get_global_id(0);
+  result_g[gid] = first_argument_g[gid] + second_argument_g[gid];
+}
--- quick_test.py
+++ quick_test.py
@@ -4,7 +4,9 @@
 #   - do a very small workload test on each device
 
 import pyopencl as cl
+import numpy as np
 
+# Identify available OpenCL platforms and devices
 platforms = cl.get_platforms()
 print('\n')
 print('OpenCL platform and device information...')
@@ -22,3 +24,53 @@ for platform in platforms:
         print('    ', 'Global memory: ', device.global_mem_size)
         print('    ', 'Work group size: ', device.max_work_group_size)
         print('    ', 'Compute units: ', device.max_compute_units)
+
+
+# Run small workloads on each device
+# platforms = cl.get_platforms()
+# for platform in platforms:  
+    # devices = platform.get_devices(cl.device_type.ALL)
+    # for device in devices:
+
+
+first_argument_np = np.random.rand(5).astype(np.float32)
+second_argument_np = np.random.rand(5).astype(np.float32)
+
+context = cl.create_some_context(interactive=True)
+command_queue = cl.CommandQueue(context)
+memory_flags = cl.mem_flags
+
+first_argument_g = cl.Buffer(
+    context, 
+    memory_flags.READ_ONLY | memory_flags.COPY_HOST_PTR, 
+    hostbuf=first_argument_np)
+    
+second_argument_g = cl.Buffer(
+    context, 
+    memory_flags.READ_ONLY | memory_flags.COPY_HOST_PTR, 
+    hostbuf=second_argument_np)
+
+with open('./kernels/small_multiply.cl', 'r') as kernel_file:
+    kernel_source = kernel_file.read()
+program = cl.Program(context, kernel_source).build()
+
+result_g = cl.Buffer(
+    context, 
+    memory_flags.WRITE_ONLY, 
+    first_argument_np.nbytes)
+
+program.add(
+    command_queue, 
+    first_argument_np.shape, 
+    None, 
+    first_argument_g, 
+    second_argument_g, 
+    result_g)
+
+result_np = np.empty_like(first_argument_np)
+cl.enqueue_copy(command_queue, result_np, result_g)
+
+print('\n')
+print(' ', first_argument_np)
+print('+', second_argument_np)
+print('=', result_np)
