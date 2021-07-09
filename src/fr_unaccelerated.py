f43640cd77e4b486d35edc0c0708b142e370ce02
Miracle, Calvin B <calvin.miracle@jefferson.kyschools.us>
2021-07-09 16:38:24 -0400

    Working on debugging

--- src/fr_unaccelerated.py
+++ src/fr_unaccelerated.py
@@ -12,61 +12,40 @@ def entry(dataset_info):
     print(dataset_info)
     dataset = get_clean_data(dataset_info, dump=False)
 
-    distances = get_distances(dataset)
+    get_entropy(dataset)
     return None
 
 
-def get_distances(dataset):
+def get_entropy(dataset):
+
+    features = len(dataset[0])
+    instances = len(dataset)
 
     max_values = [max(r) for r in [list(x) for x in zip(*dataset)]]
     min_values = [min(r) for r in [list(x) for x in zip(*dataset)]]
 
-    print('\n')
-    features = len(dataset[0])
-    instances = len(dataset)
-
-
-
-    nl = '\n'
-
-    print(nl, 'dataset', dataset)
-
     diff_ds = [np.subtract(dataset[i], dataset[j]) for i in range(instances-1) for j in range(i+1, instances)]
-    print(nl, 'diff_ds', *diff_ds)
-
     diff_values = [np.subtract(max_values[k], min_values[k]) for k in range(features)]
-    print(nl, 'max_values', max_values)
-    print(nl, 'min_values', min_values)
-    print(nl, 'diff_values', diff_values)
 
-    divs = np.divide(diff_ds, diff_values)
-    print(nl, 'divs', divs)
-
-    squares = np.square(divs)
-    print(nl, 'squares', squares)
-
+    normalize = np.divide(diff_ds, diff_values)
+    squares = np.square(normalize)
     sum_sq = [np.sum(s) for s in squares]
-    print(nl, 'sum_sq', sum_sq)
-
     distances = np.sqrt(sum_sq)
-    print(nl, 'distances', distances)
 
     average_distance = np.average(distances)
-    # print(nl, average_distance)
-
     alpha = np.divide(-np.log(0.5), average_distance)
-    # print(nl, alpha)
 
     sims = np.exp(np.multiply(-alpha, distances))
     log_sims = np.log10(sims)
     dissims = np.subtract(1, sims)
     log_dissims = np.log10(dissims)
+    
+    print('\n', *sims)
 
-    es = np.add(np.multiply(sims, log_sims), np.multiply(dissims, log_dissims))
-    # print(nl, es, sep='\n')
+    entropy_pairwise = np.add(np.multiply(sims, log_sims), np.multiply(dissims, log_dissims))
+    entropy_total = - np.sum(entropy_pairwise)
 
-    entropy = - np.sum(es)
-    # print(nl, entropy, sep='\n')
+    return None
 
 if __name__ == '__main__':
 
