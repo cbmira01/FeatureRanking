64f84f3ed910c112dba0f46af2288b4f94309a6c
Calvin Miracle <cbmira01@gmail.com>
2021-07-11 18:24:01 -0400

    Better common calculations

--- src/fr_unaccelerated.py
+++ src/fr_unaccelerated.py
@@ -9,79 +9,54 @@ from prep_data import *
 
 
 def ranking_protocol(dataset_info):
-    print(dataset_info)
+    # print(dataset_info)
     dataset = get_clean_data(dataset_info, dump=False)
     # need column names (get this in prep_data)
     # dataset needs column labels
 
     # Step 1: Start with an initial full set of features (no exclusions).
-    # exclude = [width of features, all elements False]
-    
+    exclude = [False for i in range(len(dataset[0]))]
+
     while True:
-        # Step 2: Find the total entropy of the remaing dataset, and the 
+        print('\n exclude: ', exclude)
+        # sys.exit('\n\nstopped')
+
+        # Step 2: Find the total entropy of the remaing dataset, and the
         #   feature entropies of each non-excluded feature.
-        # dataset = masked(dataset, exlude)
-        # feature_entropies, total_entropy = get_all_entropies(dataset, exlude)
-    
+        dataset = [dataset[k] for k in range(len(exclude)) if exclude[k] == False]
+
+        print('\n dataset: ', dataset)
+        # sys.exit('\n\nstopped')
+
+        total_entropy, feature_entropies = get_entropies(dataset)
+
         # Step 3: Find the feature fk such that the difference between the
         #   total entropy and feature entropy for fk is minimum.
-        # entropy_difference = min(np.abs(np.subtract(total_entropy, feature_entropies)))
+        entropy_difference = np.subtract(total_entropy, feature_entropies)
+        print('\n entropy_difference: ', dataset)
+
         #       need feature index
-    
-        # Step 4: Exclude feature fk from the dataset and record it as the 
-        #   "least contributing" feature. 
+
+        # Step 4: Exclude feature fk from the dataset and record it as the
+        #   "least contributing" feature.
         # exclude[fk] = True
         # print(column name of fk)
-    
+
         # Step 5: Repeat steps 2–4 until there is only one feature in F.
         # if (there is only one unexcluded feature in exclude):
         break
 
     # Step 6: Report the last remaining feature is the "most contributing" feature.
     # print(that last remaining fk)
-    
+
     return None
-    
-    
-def get_all_entropies(dataset)    
-    
-    squares = common_calulations(dataset)
-    
-    feature_entropies = get_feature_entropies(dataset, squares)
-    total_entropy = get_dataset_entropy(dataset, squares)
-    
-    return feature_entropies, total_entropy
-    
-    
-def get_dataset_entropy(dataset, squares):
 
-    sample_distances = np.sqrt([np.sum(s) for s in squares])
 
-    average_distance = np.average(sample_distances)
-    alpha = np.divide(-np.log(0.5), average_distance)
-
-    sims = np.exp(np.multiply(-alpha, sample_distances))
-    sims = [i for i in sims if i not in [1.0]]
-    dissims = np.subtract(1, sims)
-
-    entropy_pairwise = np.add(np.multiply(sims, np.log10(sims)), np.multiply(dissims, np.log10(dissims)))
-    entropy_total = - np.sum(entropy_pairwise)
-
-    return entropy_total
-    
-    
-def get_feature_entropies(dataset, squares):
-
-    #
-
-    return feature_entropies
-    
-    
-def common_calculations(dataset)
+def get_entropies(dataset):
 
     features = len(dataset[0])
     instances = len(dataset)
-    
+
     sample_differences = [
     np.subtract(dataset[i], dataset[j])
     for i in range(instances-1)
@@ -96,8 +71,30 @@ def common_calculations(dataset)
     np.subtract(max_values[k], min_values[k])
     for k in range(features)
     ]
-    
-    return np.square(np.divide(sample_differences, value_ranges))
+
+    normalized = np.divide(sample_differences, value_ranges)
+
+    # Calculate total entropy
+    sample_distances = np.sqrt(np.sum(np.square(normalized)))
+    average_sample_distance = np.average(sample_distances)
+    alpha = np.divide(- np.log(0.5), average_sample_distance)
+
+    similarities = np.exp(np.multiply(-alpha, sample_distances))
+    similarities = [i for i in similarities if i not in [1.0]]
+    dissimilarities = np.subtract(1.0, similarities)
+
+    pairwise_entropies = 
+    np.add(
+    np.multiply(similarities, np.log10(similarities)), 
+    np.multiply(dissimilarities, np.log10(dissimilarities))
+    )
+
+    total_entropy = - np.sum(pairwise_entropies)
+
+    # Calculate feature entropies
+    x = np.absoulte(normalized)
+
+    return total_entropy, feature_entropies
 
 
 if __name__ == '__main__':
