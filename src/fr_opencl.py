96d78fccbfae681d58dd9039e8a46883e316a299
Calvin Miracle <cbmira01@gmail.com>
2021-07-17 22:35:56 -0400

    Hooked up to all datasets now

--- src/fr_opencl.py
+++ src/fr_opencl.py
@@ -9,11 +9,11 @@ import sys
 from prep_data import *
 
 def opencl_device_driver(dataset_info):
-    # breakpoint()
+
     for platform in cl.get_platforms():
         for device in platform.get_devices(cl.device_type.ALL):
             print(device, '\n')
-            ranking_protocol(dataset_info, [device])
+            ranking_protocol(dataset_info, device)
 
     return None
 
@@ -21,78 +21,32 @@ def opencl_device_driver(dataset_info):
 def ranking_protocol(dataset_info, device):
 
     # Prepare the context, command queue and program  for the current device
-    context = cl.Context(device)
-
-    with open('./kernels/feature_ranking.cl', 'r') as f:
-        kernel_source = f.read()
+    context = cl.Context([device])
 
     try: 
+        with open('./kernels/feature_ranking.cl', 'r') as f:
+            kernel_source = f.read()
+
         program = cl.Program(context, kernel_source).build()
     except:
         print('\n\n      There was an error while building the kernel...')
         e = sys.exc_info()
         print('\nError type: ', e[0])
-        print('\nError value: \n', e[1])
+        print('\nError value: \n', e[1]) # contents of .get_build_info LOG
         # print('\nError traceback: ', e[2])
         sys.exit()
 
-    dataset1 = [
-    [3, 2, 1],
-    [2, 4, 3],
-    [5, 3, 9]
-    ]
-
-    dataset2 = [
-    [0.34818205,0.0054406016,0.17633587,0.010881203,2.5290077],
-    [0.9478498,0.8079211,0.9687416,1.6158422,4.9062247],
-    [0.2887636,0.450703,0.29179972,0.901406,2.875399],
-    [0.62086165,0.59616053,0.09807767,1.1923211,2.294233],
-    [0.017327862,0.823534,0.6032455,1.647068,3.8097365],
-    [0.06374773,0.86418086,0.9055053,1.7283617,4.716516],
-    [0.33667052,0.23504795,0.6865279,0.4700959,4.0595837],
-    [0.82834285,0.35440725,0.123982616,0.7088145,2.3719478],
-    [0.92590517,0.41533554,0.62911564,0.8306711,3.887347],
-    [0.5316418,0.52066636,0.3490727,1.0413327,3.047218]
-    ]
-
-    label_names1 = [
-        "F1",
-        "F2",
-        "F3",
-    ]
-
-    label_names2 = [
-        "C1",
-        "C2",
-        "C3",
-        "C4",
-        "C5",
-        "C6",
-        "C7",
-        "C8"
-    ]
-
-    print(label_names1)
-    # print(*dataset1,sep='\n')
-    print('\n')
-    print(label_names2)
-    # print(*dataset2,sep='\n')
-
-    # ------------------------------------
-
-    dataset = dataset1
-    label_names = label_names1
-    # breakpoint()
-    # for now, call entropy calculation here
-    get_entropy_opencl(dataset, context, program)
-
-    sys.exit()
-    # ------------------------------------
+    dataset = get_clean_data(dataset_info, dump=False)
+    label_names = get_label_names(dataset_info)
 
     print('\n')
     print('Dataset: ', dataset_info['long_name'])
     print('Label names: ', label_names)
     print('OpenCL device:', device)
+
+    # for now, call entropy calculation here
+    get_entropy_opencl(dataset, context, program)
+    sys.exit()
 
     # Step 1: Start with an initial full set of features (no exclusions).
     instances = len(dataset)
@@ -146,7 +100,7 @@ def ranking_protocol(dataset_info, device):
 
 
 def get_entropy_opencl(dataset, ctx, program):
-    breakpoint()
+
     # OpenCL execution context and compiled program on hand
     mf = cl.mem_flags
     queue = cl.CommandQueue(ctx)
@@ -164,6 +118,7 @@ def get_entropy_opencl(dataset, ctx, program):
     # Sample Differences
     dataset_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=dataset_np)
     program.sample_differences(queue, dataset_np.shape, None, dataset_g, np.int32(3), np.int32(3), sample_differences_g)
+    breakpoint()
     cl.enqueue_copy(queue, sample_differences_np, sample_differences_g)
 
     # ------------------------------------------------------------------
