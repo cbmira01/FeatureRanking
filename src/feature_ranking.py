b9b45f7a32e3637acb9767e333013dfcc2c1fd8b
Calvin Miracle <cbmira01@gmail.com>
2021-08-27 10:29:11 -0400

    Work on main program

--- src/feature_ranking.py
+++ src/feature_ranking.py
@@ -1,37 +1,20 @@
 
 #
 # This is the main program of the Feature Ranking project.
-# It alows the user to list the datasets and choose one for a trial run.
+# It alows the user to list datasets and OpenCL device, and prepare for a trial run.
 #
 
 import sys
 import trial_runner
-from prepare_data import *
+from data_handler import *
+from device_handler import *
 
 
 def list_datasets():
+ 
     print('\nDatasets to choose from...\n')
-    for ds in datasets_list:
-        print('    {}{}'.format(ds['short_name'].ljust(10), ds['long_name']))
-    return None
 
-
-def list_datasets_with_credits():
-    for ds in datasets_list:
-        short_name = ds['short_name']
-        credits_file = '../data/' + short_name + '/credit.txt'
-        with open(credits_file) as file:
-            credit_text = file.read()
-
-        print('\n========================================================')
-        print('{}{}'.format(short_name.ljust(10), ds['long_name']), end='')
-        print('\n', ds['abstract'])
-        print('\n', credit_text)
-    return None
-
-
-def describe_datasets():
-    for ds in datasets_list:
+    for ds in datasets:
         short_name = ds['short_name']
         print('\n========================================================')
         print('{}{}'.format(short_name.ljust(10), ds['long_name']))
@@ -42,32 +25,16 @@ def describe_datasets():
         print('    remove_attributes: ', ds['remove_attributes'])
         print('    remove_instances: ', ds['remove_instances'])
 
+        ic = (instances ** 2 - instances) / 2
+        print('    This dataset will require ', ic, ' comparisons bewteen instances')
+
+
+    # also list number of elementwise comparisons
+
     return None
 
 
-def run_trial():
-
-
-    # TODO: pick a dataset
-    #       pick a processor
-    #       build a trial_context
-    #       start the trial
-
-    print('\nRun a trial on one of the following datasets: \n')
-
-    for ds in datasets_list:
-        print(ds['short_name'], '  ', end='')
-
-    ds_name = input('\n\nPlease chose a dataset name: ').lower().strip()
-    ds_info = next((d for d in datasets_list if d['short_name'] == ds_name), None)
-
-    if ds_info is not None:
-        trial_runner.start(ds_info)
-
-
-
-
-# def opencl_device_driver(dataset_info):
+def list_devices(dataset_info):
 
     # for platform in cl.get_platforms():
         # for device in platform.get_devices(cl.device_type.ALL):
@@ -78,35 +45,55 @@ def run_trial():
     return None
 
 
+def run_trial():
+
+    # new trial context trial_context = {}
+
+    print('\nRun a trial... \n')
+
+    print('\nChoose a dataset: \n')
+
+    for ds in datasets:
+        print(ds['short_name'], '  ', end='')
+
+    ds_name = input('\n\nPlease chose a dataset name: ').lower().strip()
+    ds_info = next((d for d in datasets_list if d['short_name'] == ds_name), None)
+
+    if ds_info is not None:
+        trial_runner.start(ds_info)
+
+    print('\nChoose an OpenCL device: \n')
+
+    # trial_runner(trial_context)
+
 def exit_program():
     print('\nThank you for using Feature Ranking...')
     sys.exit()
     return None
 
 
-def switch_on(c):
+def switch_on(c.lower().strip()):
     switcher = {
-        'list': list_datasets,
-        'describe': describe_datasets,
-        'credits': list_datasets_with_credits,
-        'trial': run_trials,
+        'datasets': list_datasets,
+        'devices': list_devices,
+        'trial': zzz,
         'exit': exit_program
     }
     return switcher.get(c, lambda: 'Invalid')()
 
 
-datasets_list = discover_datasets()
+datasets = discover_datasets()
+devices = discover_opencl_devices()
 
 # Main command loop
 while True:
     print('\n')
     print('FEATURE RANKING main menu')
     print()
-    print('   list ------ List available datasets')
-    print('   describe -- Describe available datasets')
-    print('   credits --- List datasets with credits')
+    print('   datasets -- List available datasets')
+    print('   devices --- List available OpenCL devices')
     print('   trial ----- Run feature ranking trials on a dataset')
     print('   exit ------ Exit')
     print()
 
-    switch_on(input('Choice? ').lower().strip())
+    switch_on(input('Choice? '))
